2f1ac90330e1b7609b210ffef925a525740d6b90
Greg <contact@greghenry.xyz>
2020-09-30 01:22:54 +0200

    Experience contents added

--- src/script.js
+++ src/script.js
@@ -1,6 +1,20 @@
-$(function(){
-    $('[id$="include "]').each(function (e){
-	$(this).load("src/" + $(this).attr("id").replace("include ", "") +  ".html");
-    });
-    console.log("src/" + $(this).attr("id").replace("include ", "") +  ".html")
+function addScrollEmptySpace() {
+    var scrollableContainer = document.querySelector(".grid.body .experiences .text.heading");
+    var hasVerticalScroll = scrollableContainer.scrollHeight > scrollableContainer.clientHeight;
+    if (hasVerticalScroll) {
+	var scrollbarWidth = scrollableContainer.offsetWidth - scrollableContainer.clientWidth;
+    }
+    var emptySpace = scrollableContainer.offsetWidth - scrollableContainer.scrollWidth;
+    if (scrollbarWidth > emptySpace) {
+	scrollbarWidth = emptySpace;
+    }
+    scrollableContainer.style.minWidth = scrollbarWidth + 'px';
+}
+
+$(document).ready(function() {
+    // $(".announce .text.main").load("src/header-announce-text.html");
+    addScrollEmptySpace();
+    window.addEventListener('resize', addScrollEmptySpace, false);
 });
+
+$("a.extern").attr({target:"_black", rel:"noopener noreferrer"});
